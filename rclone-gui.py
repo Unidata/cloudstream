34ce66b66fb012d1fab19479c78859a1a71e8a4b
Ward Fisher <wfisher@ucar.edu>
2016-05-17 11:14:38 -0600

    Making a few steps forward.

--- rclone-gui.py
+++ rclone-gui.py
@@ -1,40 +1,41 @@
 #!/usr/bin/python3
 
-import tkinter
+import tkinter as tk
 from tkinter import messagebox
-
+from tkinter import Button
 import subprocess
 
-top = tkinter.Tk()
+class MainApplication(tk.Frame):
+        def __init__(self, parent, *args, **kwargs):
+                tk.Frame.__init__(self, parent, *args, **kwargs)
+                parent.minsize(width=150,height=100)
+                self.parent = parent
+                # Create GUI here
+                self.B = Button(self, text="Hello", command = self.configDB)
+                self.C = Button(self, text="cmd", command = self.tst2)
 
-def quit(event):
-        print("Quit event caught")
-        top.quit()
+                self.B.pack(side="top",fill="x",expand=True)
+                self.C.pack(side="top",fill="x",expand=True)
 
-def tst2():
-        stdoutdata = subprocess.getoutput("ls")
-        messagebox.showinfo("Outpout",stdoutdata.split()[0])
+                parent.bind('<Control-c>', self.quit_proc)
 
-def tst():
-        p = subprocess.Popen("echo hi", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        stdout = []
-        while True:
-                line = p.stdout.readline()
-                stdout.append(line)
-                print(line),
-                if line == '' and p.poll() != None:
-                   break
+        def configDB(self):
+                messagebox.showinfo("Hello Python", "Hello World")
 
-        messagebox.showinfo("Contents",''.join(stdout))
+        def quit_proc(self):
+                print("Quit event caught")
+                quit()
 
-def configDB():
-        messagebox.showinfo("Hello Python", "Hello World")
+        def tst2(self):
+                stdoutdata = subprocess.getoutput("ls")
+                messagebox.showinfo("Outpout",stdoutdata.split()[0])
 
-B = tkinter.Button(top, text="Hello", command = configDB)
-C = tkinter.Button(top, text="cmd", command = tst2)
 
-B.pack()
-C.pack()
 
-top.bind('<Control-c>', quit)
-top.mainloop()
+
+
+if __name__ == "__main__":
+        root = tk.Tk()
+        MainApplication(root).pack(side="top", fill="both", expand=True)
+
+        root.mainloop()
